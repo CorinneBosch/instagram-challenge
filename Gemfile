fdc9e4a9df5d39746773c92f991b97ee5b6dd26c
Corinne <corinne.boesch@gmail.com>
2021-09-21 15:02:06 +0100

    implementing gemoji

--- Gemfile
+++ Gemfile
@@ -23,7 +23,8 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+# Character information and metadata for standard and custom emoji
+gem 'gemoji', '~> 3.0', '>= 3.0.1'
 # Use Active Storage variant
 gem 'image_processing', '~> 1.2'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :emojify
+
+  def emojify(content)
+    (content).to_str.gsub(/:([\w+-]+):/) do |match|
+      if emoji = Emoji.find_by_alias($1) { nil }
+        %(<img alt="#$1" src="../public/images/emoji/#{emoji.image_filename}"
+        style="vertical-align:middle" width="20" height="20" />)
+      else
+        match
+      end
+    end.html_safe if content.present?
+  end
 end
